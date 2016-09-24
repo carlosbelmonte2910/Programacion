93c4f1b7a175192e0ac7d7f7aa040e1d7de9dd45
carlosbelmonte2910 <carlos.m.belmonte@gmail.com>
2016-09-24 17:54:41 -0300

    Programacion en C#, entorno Visual Studio

---
+++ Visual Studio/Superficie cuadrado/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado;
+            int superficie;
+            String linea;
+            Console.Write("Ingrese el valor del lado:");
+            linea = Console.ReadLine();
+            lado = int.Parse(linea);
+            superficie = lado * lado;
+            Console.Write("El valor de la superficie es:");
+            Console.Write(superficie);
+            Console.ReadKey();
+        }
+    }
+}
