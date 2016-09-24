6f49ff2a6b0d350c12551df050b204b907543900
carlosbelmonte2910 <carlos.m.belmonte@gmail.com>
2016-09-24 18:03:48 -0300

    Programacion en C#, entorno Visual Studio

---
+++ Visual Studio/Encender-Apagar led/Encender-Apagar LED/Encender-Apagar LED/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Encender_Apagar_LED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox_visualizar_mensaje = new System.Windows.Forms.TextBox();
+            this.label_mensaje_arduino = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1 : HIGH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2: LOW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM12";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(59, 17);
+            this.toolStripStatusLabel1.Text = "hh:mm:ss";
+            // 
+            // textBox_visualizar_mensaje
+            // 
+            this.textBox_visualizar_mensaje.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox_visualizar_mensaje.Location = new System.Drawing.Point(0, 212);
+            this.textBox_visualizar_mensaje.Multiline = true;
+            this.textBox_visualizar_mensaje.Name = "textBox_visualizar_mensaje";
+            this.textBox_visualizar_mensaje.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_visualizar_mensaje.Size = new System.Drawing.Size(284, 28);
+            this.textBox_visualizar_mensaje.TabIndex = 3;
+            // 
+            // label_mensaje_arduino
+            // 
+            this.label_mensaje_arduino.AutoSize = true;
+            this.label_mensaje_arduino.Location = new System.Drawing.Point(9, 196);
+            this.label_mensaje_arduino.Name = "label_mensaje_arduino";
+            this.label_mensaje_arduino.Size = new System.Drawing.Size(117, 13);
+            this.label_mensaje_arduino.TabIndex = 4;
+            this.label_mensaje_arduino.Text = "Mensaje desde arduino";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label_mensaje_arduino);
+            this.Controls.Add(this.textBox_visualizar_mensaje);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox_visualizar_mensaje;
+        private System.Windows.Forms.Label label_mensaje_arduino;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Visual Studio/Encender-Apagar led/Encender-Apagar LED/Encender-Apagar LED/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Encender_Apagar_LED
+{
+    public partial class Form1 : Form
+    {
+        //Utilizamos un string como Buffer de recepcion
+        string Recibidos;
+        public Form1()
+        {
+            InitializeComponent();
+            //Abrir el puerto mientras se ejecute aplicacion
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            //Ejecutar la funcion Recepcion por disparo del Evento 'DataReceived'
+            serialPort1.DataReceived += new
+            System.IO.Ports.SerialDataReceivedEventHandler(Recepcion);
+        }
+        //Al Recibir datos
+        private void Recepcion(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            //Acumular los caracteres recibidos a nuestro Buffer (string)
+            Recibidos += serialPort1.ReadExisting();
+            //Invocar o llamar la proceso de tramas
+            this.Invoke(new EventHandler(Actualizar));
+        }
+        //Procesar los datos recibidos en el buffer y extraer tramas completas
+        private void Actualizar(object s, EventArgs e)
+        {
+            //Asignar el valor a la trama del texto
+            textBox_visualizar_mensaje.Text = Recibidos;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] mBuffer = new byte[1];
+            mBuffer[0] = 0x68; //Letra h en ascii
+            serialPort1.Write(mBuffer, 0, mBuffer.Length);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] mBuffer = new byte[1];
+            mBuffer[0] = 0x6C; //Letra l en ascii
+            serialPort1.Write(mBuffer, 0, mBuffer.Length);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            statusStrip1.Items[0].Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
